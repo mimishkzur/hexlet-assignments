516257bf928e1332a67566990725a9ff320edbf4
@hexlet/cli <support@hexlet.io>
2025-03-12 19:22:43 +0300

    submit java-spring-boot-ru/annotations

--- java-spring-boot-ru/annotations/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/annotations/src/main/java/exercise/Application.java
@@ -19,7 +19,7 @@ public class Application {
                     e.printStackTrace();
                 }
 
-                System.out.println("Method " + method.getName() + " returns a value of type " + method.getReturnType().toString());
+                System.out.println("Method " + method.getName() + " returns a value of type " + method.getReturnType().getSimpleName());
             }
         }
         // END
