dd898353161db25476d7a36f41f016cc58898a40
@hexlet/cli <support@hexlet.io>
2025-03-14 15:14:30 +0300

    submit java-spring-boot-ru/rest-api

--- java-spring-boot-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
+++ java-spring-boot-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -1,10 +1,7 @@
 package exercise.controller.users;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,10 +12,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import exercise.model.Post;
 import exercise.Data;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 // BEGIN
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class PostsController {
 
     private List<Post> posts = Data.getPosts();
@@ -27,7 +24,7 @@ public class PostsController {
     public List<Post> show(@PathVariable Integer id) {
 
         var post = posts.stream()
-                .filter(p -> p.getUserId().equals(id))
+                .filter(p -> p.getUserId() == id)
                 .toList();
         return post;
     }
